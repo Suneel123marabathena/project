13167c828bb4ba46e3bf6e568765515bbeb3ad76
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-03 23:31:54 +0530

    Second Practice

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = 100;
+        short s = 200;
+        long l = 9390000829l;
+        float f = 10.1f;
+        double d = 100.53;
+        char ch = 'A';
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(Byte.SIZE);
+        System.out.println(a);
+        System.out.println(Short.SIZE);
+        System.out.println(Double.SIZE);
+    }
+}
