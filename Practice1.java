ce716810e3bfdc21f366e5bd0707dd0a2d52cdc0
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-04 11:33:07 +0530

    Weekdays

---
+++ Practice1.java
@@ -0,0 +1,11 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
