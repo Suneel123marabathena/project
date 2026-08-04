929e7cca72de898559670e73c010774f2d3e1db6
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-04 12:27:05 +0530

    Practice problem two

---
+++ Practice2.java
@@ -0,0 +1,12 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num == 0) {
+            System.out.println("Zero");
+        } else if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
