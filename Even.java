de6e9473562acc0339297dfe7d240666d8811e37
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-04 21:30:34 +0530

    checking even or odd

---
+++ Even.java
@@ -0,0 +1,10 @@
+public class Even {
+    public static void main(String[] args) {
+        int num = 20;
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
