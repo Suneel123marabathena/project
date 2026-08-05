a7355b8a60fdf0fcb333105fed96d7d658d1abd5
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-05 12:22:19 +0530

    printing even using loop

--- Loop.java
+++ Loop.java
@@ -1,6 +1,8 @@
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
+        // for (int i = 0; i <= 5; i++) {
+        // System.out.println(i);
+        for (int i = 0; i <= 10; i += 2) {
             System.out.println(i);
         }
     }
