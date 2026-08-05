74f9c7e20f9efe00d93fb33a2baa1e4e58b1479f
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-05 21:52:46 +0530

    practice 3

---
+++ practice3.java
@@ -0,0 +1,8 @@
+public class practice3 {
+    public static void main(String[] args) {
+        int num = 100;
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i);
+        }
+    }
+}
