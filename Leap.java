9a9abf26bc04673410c41f63f1adf977f7be1f35
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-05 22:17:05 +0530

    leap year

---
+++ Leap.java
@@ -0,0 +1,11 @@
+public class Leap {
+    public static void main(String[] args) {
+        int year = 2026;
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("leap year");
+
+        } else {
+            System.out.println("Normal year");
+        }
+    }
+}
