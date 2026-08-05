c2d1c6b1dc0b6525c3338812e94535660124770b
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-05 22:04:06 +0530

    practice 6

---
+++ practice5.java
@@ -0,0 +1,8 @@
+public class practice5 {
+    public static void main(String[] args) {
+        int n = 20;
+        for (int i = 0; i <= n; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ practice6.java
@@ -0,0 +1,16 @@
+public class practice6 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        int c = 60;
+
+        if (a > b && a > c) {
+            System.out.println("a is greater");
+        } else if (b > a && b > c) {
+            System.out.println("b is greater");
+            ;
+        } else {
+            System.out.println("c is greater");
+        }
+    }
+}
