be1fef36245af23d892591e3d77a9d673f1f9d33
marabathena suneel <suneelmarabathena@gmail.com>
2026-08-07 12:01:54 +0530

    character type

---
+++ character1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class character1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the character:");
+        char ch = scan.next().charAt(2);
+        System.out.println(ch);
+    }
+}
